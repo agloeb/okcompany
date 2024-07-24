d448788162bea57fb5dfa6c2778116a87057e347
agloeb <147872579+agloeb@users.noreply.github.com>
2024-07-24 08:48:20 -0600

    Update operator.js

--- operator.js
+++ operator.js
@@ -6,20 +6,18 @@ function mouseover(x){
                     };
                 }}
         function mouseleave() {
-            document.getElementById("base-image").src="img/OPERATORbkg.png";
+            document.getElementById("base-image").src="img/operator/OPERATORbkg.png";
             }
 
             document.addEventListener('DOMContentLoaded', function() {
             const images = document.querySelectorAll('.toggle-image');
 
             images.forEach(image => {
-                // Set the original source attribute initially if not already set
                 if (!image.hasAttribute('data-original-src')) {
                     image.setAttribute('data-original-src', image.src);
                 }
 
                 image.addEventListener('click', function() {
-                    // Get the current and alternate image sources
                     const currentSrc = image.src;
                     const originalSrc = image.getAttribute('data-original-src');
                     const altSrc = image.getAttribute('data-alt-src');
