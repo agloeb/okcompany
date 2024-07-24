573f1ee2e47abd9eedeb10ecb93c8ad591bf8c99
agloeb <147872579+agloeb@users.noreply.github.com>
2024-07-23 19:42:03 -0600

    Add files via upload

---
+++ manuals.js
@@ -0,0 +1,36 @@
+function mouseover(x){
+            switch (x) {
+                case 'ok':
+                    {
+                    document.getElementById("base-image").src="img/manuals/off.png"
+                    };
+                }}
+        function mouseleave() {
+            document.getElementById("base-image").src="img/manuals/off.png";
+            }
+
+            document.addEventListener('DOMContentLoaded', function() {
+            const images = document.querySelectorAll('.toggle-image');
+
+            images.forEach(image => {
+                if (!image.hasAttribute('data-original-src')) {
+                    image.setAttribute('data-original-src', image.src);
+                }
+
+                image.addEventListener('click', function() {
+                    const currentSrc = image.src;
+                    const originalSrc = image.getAttribute('data-original-src');
+                    const altSrc = image.getAttribute('data-alt-src');
+
+                    console.log(`Click on ${image.id}: currentSrc=${currentSrc}, originalSrc=${originalSrc}, altSrc=${altSrc}`);
+
+                    if (currentSrc.endsWith(altSrc)) {
+                        image.src = originalSrc;
+                    } else {
+                        image.src = altSrc;
+                    }
+
+                    console.log(`After click: newSrc=${image.src}`);
+                });
+            });
+        });
