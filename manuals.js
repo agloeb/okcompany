1a21cab27020414b72e188ccda9b9b655d95e27f
agloeb <147872579+agloeb@users.noreply.github.com>
2024-07-30 14:17:50 -0600

    Update manuals.js

--- manuals.js
+++ manuals.js
@@ -49,3 +49,21 @@ function toggleImg(id) {
     var image = document.getElementById(id);
     image.style.display = 'block';
 }
+
+function toggleAllOff() {
+    var textBlocks = document.querySelectorAll('.text-block');
+    var imageBlocks = document.querySelectorAll('.img-block');
+
+    textBlocks.forEach(function(block) {
+        block.style.display = 'none';
+    });
+
+    imageBlocks.forEach(function(block) {
+        block.style.display = 'none';
+    });
+    
+    var claygutImage = document.querySelector('.toggle-image');
+    if (claygutImage) {
+        claygutImage.src = 'img/manuals/claygutOFF.png';
+    }
+}
