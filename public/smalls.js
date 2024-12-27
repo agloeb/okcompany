33c7a60fdf06c446d4f5e0c6db7270099b099f5e
agloeb <147872579+agloeb@users.noreply.github.com>
2024-12-27 09:06:50 -0700

    Update smalls.js

--- public/smalls.js
+++ public/smalls.js
@@ -1,4 +1,4 @@
-setTimeout(function(){ window.location = '/public/windmill.html'; }, 300000)
+setTimeout(function(){ window.location = '/public/windmill.html'; }, 600000)
 
 function toggleText(textId) {
   var textBlocks = document.querySelectorAll('.text-block');
